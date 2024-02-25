16d3eaf246c2d0a7793dde061425aae6c76b657a
LENOVO  PC <LENOVO  PC@DESKTOP-AKDJE3T>
2024-02-24 21:54:22 -0500

    Second Commit

--- Project/src/Sample.java
+++ Project/src/Sample.java
@@ -1,8 +1,10 @@
 
-public class Sample {
+public class Sample
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+{
+		System.out.println("Hello!");
 
 	}
 
